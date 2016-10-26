0636c2782e3fc20c9bac2e7f1045419d7b708a1d
Daniel Marcos <danielms215@gmail.com>
2016-10-25 20:28:36 -0400

    Added better password input (so that password isn't displayed on terminal). Changed print statements to print(), better exception messages

--- gbunity.py
+++ gbunity.py
@@ -31,7 +31,7 @@ def loginToT2(username, password):
 			EC.presence_of_element_located((By.NAME, "username"))
 		)
 	except TimeoutException:
-		print "Loading took too long fam"
+		print("Loading took too long")
 
 	# Make sure username and password are empty
 	browser.find_element_by_name("username").clear()
@@ -53,7 +53,7 @@ def getCurrentClasses():
 			EC.presence_of_element_located((By.ID, "siteLinkList"))
 			)
 	except TimeoutException:
-		print "Loading too hella long fam"
+		print("Loading took too long")
 
 	# Clicks on "Active Sites"
 	browser.execute_script("return dhtml_more_tabs();")
@@ -75,8 +75,8 @@ def processGradebooks(classLinks):
 		browser.get(link)
 		classTitle = browser.find_elements_by_css_selector(".selectedTab")[0].find_element_by_tag_name("a").find_element_by_tag_name("span").text
 
-		print " \n -----------------------------------------"
-		print classTitle, "\n"
+		print(" \n -----------------------------------------")
+		print(classTitle + "\n")
 
 		toolMenu = browser.find_element_by_id("toolMenu")
 		for elem in toolMenu.find_elements_by_tag_name("a"):
@@ -95,6 +95,7 @@ def processTable():
 	for t in browser.find_elements_by_tag_name("table"):
 		if t.get_attribute("class") == "listHier wideTable lines":
 			gbTable = t
+	time.sleep(1)
 	if gbTable:
 		# Classes have different things on the gradebook so we need to know
 		# what position contains title and what position contains grade
@@ -118,7 +119,7 @@ def processTable():
 		# Print title and grade
 		for row in rows:
 			rowData = row.find_elements_by_tag_name("td")
-			print rowData[gradePos].text, " --- ", rowData[titlePos].text
+			print(rowData[gradePos].text + " --- " + rowData[titlePos].text)
 			
 		browser.switch_to_default_content()
 
@@ -127,8 +128,8 @@ def createCombinedGradebook():
 	processGradebooks(getCurrentClasses())
 
 if __name__ == '__main__':
-	if (USERNAME == "" and PASSWORD == ""):
+	if USERNAME == "" and PASSWORD == "":
+		import getpass
 		USERNAME = raw_input("Enter your GT Username: ") 
-		PASSWORD = raw_input("Enter your password: ")
-	
+		PASSWORD = getpass.getpass("Enter your password: ")
 	createCombinedGradebook()
